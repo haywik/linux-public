e66300f6aeea1292ed67f162d47a514b705fbee9
haywik <nobodysgamer11@gmail.com>
2026-02-08 18:16:19 +0000

    Update full_build_xmrig.sh

--- crypto/full_build_xmrig.sh
+++ crypto/full_build_xmrig.sh
@@ -30,19 +30,19 @@ cd $dir_set
 echo "nice -n 19 $dir_set/xmrig/build/xmrig -o gulf.moneroocean.stream:10001 -u $address -p $host" > run.sh
 
 { 
-echo "[Unit]" 
-echo "Description=Monero miner service"
-echo " "
-echo "[Service]"
-echo "ExecStart=$dir_set/xmrig/run.sh"
-echo "Restart=always"
-echo "Nice=19"
-echo "CPUWeight=1"
-echo "User=$user"
-echo "Type=simple"
-echo " "
-echo "[Install]"
-echo "WantedBy=multi-user.target"
+sudo echo "[Unit]" 
+sudo echo "Description=Monero miner service"
+sudo echo " "
+sudo echo "[Service]"
+sudo echo "ExecStart=$dir_set/xmrig/run.sh"
+sudo echo "Restart=always"
+sudo echo "Nice=19"
+sudo echo "CPUWeight=1"
+sudo echo "User=$user"
+sudo echo "Type=simple"
+sudo echo " "
+sudo echo "[Install]"
+sudo echo "WantedBy=multi-user.target"
 } > /etc/systemd/system/monerohaywik_miner.service
 
 sudo killall xmrig
