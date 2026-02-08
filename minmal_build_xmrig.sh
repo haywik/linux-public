4ac96ce18417a664526a44c686b23e7b00467ecb
haywik <nobodysgamer11@gmail.com>
2026-02-08 17:39:17 +0000

    Create minmal_build_xmrig.sh

---
+++ minmal_build_xmrig.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+source wallet.key
+host=$(hostname)
+user=$(whoami)
+dir_set=/home/$user/monero-haywik
+apt-get update && sudo apt-get upgrade
+apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+
+echo "Removing any old scripts"
+killall xmrig
+rm -rf $dir_set
+
+mkdir -p /home/$user/monero-haywik
+cd $dir_set
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+
+cd $dir_set
+echo "nice -n 19 $dir_set/xmrig/build/xmrig -o gulf.moneroocean.stream:10001 -u $address -p $host" > run.sh
