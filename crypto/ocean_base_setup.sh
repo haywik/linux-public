5d1ecacc2d5ca1011a1ff0ee5ce58da52111e99d
haywik <nobodysgamer11@gmail.com>
2026-02-09 18:20:54 +0000

    Update ocean_base_setup.sh

--- crypto/ocean_base_setup.sh
+++ crypto/ocean_base_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source config.txt
+address=$(cat wallet.key)
 
 curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s $address
