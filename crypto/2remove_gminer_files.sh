0a1078ec03b2b1abc7aed181d61091d6948e1011
haywik <nobodysgamer11@gmail.com>
2026-02-09 18:15:41 +0000

    Create 2remove_gminer_files.sh

---
+++ crypto/2remove_gminer_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "run this in the location of gminer not, from the script location"
+ls
+rm -rf gminer*
+rm miner
+rm public.gpg
+rm readme.txt
+rm run.sh
+rm sample_config.txt
+ls
