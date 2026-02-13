724ab1b445c566ceef18c0acf528668d0b8eab16
haywik <nobodysgamer11@gmail.com>
2026-02-13 21:29:00 +0000

    Update checker.sh

--- web/checker.sh
+++ web/checker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 source config.txt
+source config_back.txt
 set -x
 
 for i in ${names[@]}; do
+	dir=$dir_base"/$i"
     echo "checking $i"
 
     echo "checking HTML"
@@ -12,10 +14,10 @@ for i in ${names[@]}; do
             break
         fi
     done
-    curl -s localhost:${names_port[$e]}/home | head -5
+    curl -s ${names_port[$e]}/home | head -5
 
     echo "updating the git"
-    runuser -l gitter-$i -c "bash /home/$i/auto/git.sh"
+    runuser -l gitter-$i -c "bash $dir/auto/git.sh"
 
     echo "doing systemtl stuff"
 	systemctl restart startup-$i --no-pager
