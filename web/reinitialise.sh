75bbcee0fa5d7668b3f5dfabacb016249b4bde40
haywik <nobodysgamer11@gmail.com>
2026-04-02 20:53:08 +0100

    Replace undo_full_web_server.sh with uninstall.sh

--- web/reinitialise.sh
+++ web/reinitialise.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-sudo bash ./cleanup/undo_full_web_server.sh
+sudo bash ./cleanup/uninstall.sh
 bash git update-index --skip-worktree ./config.txt && git fetch && git pull
 sudo bash web_server_full.sh
