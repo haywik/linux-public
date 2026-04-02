c326361a755d1d0717b17f2921e2335b54d7b256
haywik <nobodysgamer11@gmail.com>
2026-04-02 20:54:21 +0100

    Update reinitialise.sh

--- web/reinitialise.sh
+++ web/reinitialise.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -x
+
+if [[ $(/usr/bin/id -u) -e 0 ]]; then
+    echo "Dont run as sudo, as it changes the ownership in the git"
+    exit
 sudo bash ./cleanup/uninstall.sh
 bash git update-index --skip-worktree ./config.txt && git fetch && git pull
 sudo bash web_server_full.sh
