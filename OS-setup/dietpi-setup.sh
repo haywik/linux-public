620557ae83ebc6cb55453c19fc92d2b454cadbf4
haywik <nobodysgamer11@gmail.com>
2026-09-24 19:21:47 +0100

    Remove SSH key setup from dietpi-setup.sh
    Removed the authorized_key variable and related commands for SSH key setup.

--- OS-setup/dietpi-setup.sh
+++ OS-setup/dietpi-setup.sh
@@ -2,8 +2,6 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-export authorized_key=""
-
 useradd "haywik" -U -G sudo -m -s /bin/bash -c "primary user"
 apt-get -y update
 apt-get -y upgrade
@@ -18,8 +16,5 @@ cat > /etc/default/dropbear << EOL
 DROPBEAR_PORT=22
 DROPBEAR_EXTRA_ARGS="-s -g -l haywik"
 EOL
-
-runuser -l haywik -c "mkdir -p /home/haywik/.ssh/"
-echo "$authorized_key" >> /home/haywik/.ssh/authorized_keys
 
 systemctl restart dropbear
