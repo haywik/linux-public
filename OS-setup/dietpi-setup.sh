f6f60eec0cdd4cb4aec24443145eb843d7cdfa3f
haywik <nobodysgamer11@gmail.com>
2026-09-24 19:24:10 +0100

    Clean up dietpi-setup.sh by removing empty lines
    Removed unnecessary empty lines before creating .ssh directory and updating authorized_keys.

--- OS-setup/dietpi-setup.sh
+++ OS-setup/dietpi-setup.sh
@@ -17,8 +17,6 @@ DROPBEAR_PORT=22
 DROPBEAR_EXTRA_ARGS="-s -g -l haywik"
 EOL
 
-
-
 runuser -l haywik -c "mkdir -p /home/haywik/.ssh/"
 runuser -l haywik -c "echo >> /home/haywik/.ssh/authorized_keys"
 cp /boot/authorized_keys /home/haywik/.ssh/authorized_keys
